8095de5a078619a29eca11fa7147a315b6b3c81c
hydaline <hydaline-code>
2023-03-27 11:41:28 +0100

    Linter for Eslint checks okay

--- jscript/about.js
+++ jscript/about.js
@@ -4,5 +4,4 @@ const hamburger = document.querySelector('.hambuger');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   Menu.classList.toggle('active');
-  
 });
